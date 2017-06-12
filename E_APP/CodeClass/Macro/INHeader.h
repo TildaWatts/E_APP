d812ecc33aead8db186bc3ea1bf983b1e155cef1
TildaWatts <215722591@qq.com>
2017-06-12 16:13:59 +0800

    基本框架

--- E_APP/CodeClass/Macro/INHeader.h
+++ E_APP/CodeClass/Macro/INHeader.h
@@ -18,8 +18,6 @@
 //刷新控件
 //#import "MJRefresh.h"
 
-//缓存图片
-#import "UIImageView+WebCache.h"
 
 //拉伸图片
 //#import "UIImage+Resizable.h"
@@ -30,11 +28,6 @@
 //验证手机号 密码
 //#import "RegularExpression.h"
 
-//签名加密
-#import "NSString+hash.h"
-
-//归档及模型转换
-#import "MJExtension.h"
 
 //计算文本大小
 //#import "NSString+TextSize.h"
@@ -57,33 +50,47 @@
 #import "XResponse.h"
 
 
+//  ***  分类  *** //
+
+//缓存图片
+#import "UIImageView+WebCache.h"
+//签名加密
+#import "NSString+hash.h"
+
+
 #import "UIView+CWExtension.h"
-//#import <MJExtension.h>
-//#import <MJRefresh.h>
-
-
-//#import <SVProgressHUD.h>
-#import "FMDB.h"
-//#import "USDataTool.h"
-//#import "USProgressHud.h"
-#import "Masonry.h"
-
-#import <AFNetworking/AFNetworking.h>
-//#import "RacAnyTest-Swift.h"
-//#import <FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h>
-#import <ReactiveCocoa/ReactiveCocoa.h>
 #import "UIView+STLayout.h"
 
+// 第三方框架
+#import <MJRefresh/MJRefresh.h>
+
+#import <MJExtension/MJExtension.h>
+
+#import <SVProgressHUD/SVProgressHUD.h>
+
+#import <FMDB/FMDB.h>
+
+#import <Masonry/Masonry.h>
+
+#import <AFNetworking/AFNetworking.h>
+
+#import <FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h>
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+#import <TMCache/TMCache.h>
+
+#import <YYModel/YYModel.h>
+
+#import <CYLTabBarController/CYLTabBarController.h>
+
+
+
 // 用户相关类
-//#import "UserInfo.h"
 #import "ConstantHeader.h"
-//#import "STConstant.h"
-//#import "PINCache.h"
 
-// 信鸽推送服务
-//#import "XGSetting.h"
 
-#import "TMCache.h"
+
 //#import "USVerify.h"
 #import "LinkCenter.h"
 #import "LocalVcBridge.h"
